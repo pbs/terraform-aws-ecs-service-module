aa8f9c514996c1eb7fa1b8306e84965b8dfaa471
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2023-01-19 15:19:22 -0500

    Updating to use terraform-aws-sg-rule-module

--- examples/sgs/main.tf
+++ examples/sgs/main.tf
@@ -10,18 +10,18 @@ resource "aws_security_group" "mysql_sg" {
   }
 }
 
-resource "aws_security_group_rule" "mysql_egress" {
-  security_group_id = aws_security_group.mysql_sg.id
-  description       = "Allow all traffic out"
+module "mysql_egress" {
+  source = "github.com/pbs/terraform-aws-sg-rule-module?ref=0.0.1"
+
+  description = "Allow all traffic out"
+
   type              = "egress"
-  protocol          = "-1"
+  security_group_id = aws_security_group.mysql_sg.id
 
-  from_port = 0
-  to_port   = 0
 
-  cidr_blocks = [
-    "0.0.0.0/0",
-  ]
+  port               = 0
+  protocol           = "-1"
+  source_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "redis_sg" {
@@ -36,18 +36,18 @@ resource "aws_security_group" "redis_sg" {
   }
 }
 
-resource "aws_security_group_rule" "redis_egress" {
-  security_group_id = aws_security_group.redis_sg.id
-  description       = "Allow all traffic out"
+module "redis_egress" {
+  source = "github.com/pbs/terraform-aws-sg-rule-module?ref=0.0.1"
+
+  description = "Allow all traffic out"
+
   type              = "egress"
-  protocol          = "-1"
+  security_group_id = aws_security_group.redis_sg.id
 
-  from_port = 0
-  to_port   = 0
 
-  cidr_blocks = [
-    "0.0.0.0/0",
-  ]
+  port               = 0
+  protocol           = "-1"
+  source_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "memcached_sg" {
@@ -62,18 +62,18 @@ resource "aws_security_group" "memcached_sg" {
   }
 }
 
-resource "aws_security_group_rule" "memcached_egress" {
-  security_group_id = aws_security_group.memcached_sg.id
-  description       = "Allow all traffic out"
+module "memcached_egress" {
+  source = "github.com/pbs/terraform-aws-sg-rule-module?ref=0.0.1"
+
+  description = "Allow all traffic out"
+
   type              = "egress"
-  protocol          = "-1"
+  security_group_id = aws_security_group.memcached_sg.id
 
-  from_port = 0
-  to_port   = 0
 
-  cidr_blocks = [
-    "0.0.0.0/0",
-  ]
+  port               = 0
+  protocol           = "-1"
+  source_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "ingress_sg" {
@@ -92,14 +92,43 @@ module "service" {
 
   primary_hosted_zone = var.primary_hosted_zone
 
-  mysql_sg_ids     = [aws_security_group.mysql_sg.id]
-  redis_sg_ids     = [aws_security_group.redis_sg.id]
-  memcached_sg_ids = [aws_security_group.memcached_sg.id]
-
   restricted_sg = aws_security_group.ingress_sg.id
 
   organization = var.organization
   environment  = var.environment
   product      = var.product
   repo         = var.repo
+}
+
+module "ecs_to_mysql" {
+  source = "github.com/pbs/terraform-aws-sg-rule-module?ref=0.0.1"
+
+  security_group_id = aws_security_group.mysql_sg.id
+
+  description = "Allow service ${module.service.name} to access MySQL"
+
+  port                     = 3306
+  source_security_group_id = module.service.service_sg
+}
+
+module "ecs_to_redis" {
+  source = "github.com/pbs/terraform-aws-sg-rule-module?ref=0.0.1"
+
+  security_group_id = aws_security_group.redis_sg.id
+
+  description = "Allow service ${module.service.name} to access Redis"
+
+  port                     = 6379
+  source_security_group_id = module.service.service_sg
+}
+
+module "ecs_to_memcached_sg" {
+  source = "github.com/pbs/terraform-aws-sg-rule-module?ref=0.0.1"
+
+  security_group_id = aws_security_group.memcached_sg.id
+
+  description = "Allow service ${module.service.name} to access Memcached"
+
+  port                     = 11211
+  source_security_group_id = module.service.service_sg
 }
