29e3124e9fd8d6d1f6502c414fbea232382899d8
mmogos <mmogos@pbs.com>
2025-07-28 11:22:56 +0300

    CAT-23977 added dns_config block to aws_service_discovery_service

--- cloudmap.tf
+++ cloudmap.tf
@@ -2,6 +2,13 @@ resource "aws_service_discovery_service" "service" {
   count        = local.create_cloudmap_service ? 1 : 0
   name         = local.name
   namespace_id = var.namespace_id
+  dns_config {
+    namespace_id = var.namespace_id
+    dns_records {
+      ttl  = 60
+      type = "A"
+    }
+  }
 
   tags = merge(
     local.tags,
